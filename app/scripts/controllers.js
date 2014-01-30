79fd40ac04686c200e047de40e0a8b48b2708705
Will Bell <willbell@gmail.com>
2014-01-30 16:24:46 +0000

    data fetched via http service being injected directly into controller

--- app/scripts/controllers.js
+++ app/scripts/controllers.js
@@ -1,8 +1,14 @@
 var pingpongControllers = angular.module('pingpongControllers', []);
 
-pingpongControllers.controller('ScorerCtrl', ['$scope', 'Data',
-  function($scope, Data) {
-    $scope.message = { gameOdd: Data };
+pingpongControllers.controller('ScorerCtrl', ['$scope', 'Data', '$http',
+  function($scope, d, scope) {
+
+  	scope.get('data/dummy_data.json').success(function(data) {
+  			var testBool = data[0].flipped
+		    $scope.message = { gameOdd: testBool };
+		  });
+    
   }
 ]);
+
 
--- app/scripts/services.js
+++ app/scripts/services.js
@@ -1,40 +1,29 @@
 var pingpongServices = angular.module('pingpongServices', []);
 
-// pingpongServices.factory('Data', ['$http', '$q',
-//     function($http, $q) {
-//     	var delay = $q.defer();
-// 			$http.get('data/dummy_data.json'), function(recipe) {
-//       delay.resolve(recipe);
-//       console.log(recipe);
-//     }, function() {
-//       delay.reject('Unable to fetch recipe ');
-//     };
-// 		console.log(delay.promise);
-//     return delay.promise;
-// 		}
-		
-
-// ]);
-
-
 pingpongServices.factory('Data', ['$http', '$timeout',
     function($http, $timeout) {
-  		var testData = [];
-  		$http.get('data/dummy_data.json').success(function(data) {
-      	testData.push(data[0].flipped);
-      	console.log(testData);
-    	});
+    	var testData = [];
+		  $http.get('data/dummy_data.json').success(function(data) {
+		    testData.push(data[0].flipped);
+		  });
 
-    	console.log(testData)
-
-    	$timeout(function () {
+		  return $timeout(function () {
         console.log(testData);
-      }, 4000);
-    	
-    	// return false;
+      }, 40);
 		}
-		
-
 ]);
 
-
+// function($http, $q) {
+//     	var testData = [];
+//     	var defer = $q.defer();
+//     	defer.promise
+// 		    	.then(function () {
+// 		    		$http.get('data/dummy_data.json').success(function(data) {
+// 		      		testData.push(data[0].flipped);
+// 		    		});
+// 		    	});
+		    	
+//     	defer.resolve();
+//     	console.log(testData);		
+// 		}
+// ]);
