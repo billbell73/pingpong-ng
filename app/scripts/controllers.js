56f1b90fb13b956a57396f18e36cc0000c367362
Will Bell <willbell@gmail.com>
2014-01-31 17:54:10 +0000

    Got dummy game data working, with point scores in view

--- app/scripts/controllers.js
+++ app/scripts/controllers.js
@@ -5,7 +5,14 @@ pingpongControllers.controller('ScorerCtrl', ['$scope', 'Data',
 	function($scope, Data) {
 
 		Data.query(function(data){
-	 		$scope.message = { gameOdd: data[0].flipped };
+	 		$scope.score = data[0];
+	 		$scope.p1points = data[0].p1points;
+	 		$scope.p2points = data[0].p2points;
+			$scope.p1games = data[0].p1games;
+			$scope.p2games = data[0].p2games;
+			$scope.server = data[0].server;
+			console.log(data[0]);
+			console.log(data[0].p1points);
 	 	})
 
 	}
--- test/controllersSpec.js
+++ test/controllersSpec.js
@@ -20,24 +20,35 @@ describe('pingApp controllers', function() {
     beforeEach(inject(function(_$httpBackend_, $rootScope, $controller) {
       $httpBackend = _$httpBackend_;
       $httpBackend.expectGET('data/dummy_data.json').
-          respond([{flipped: false}]);
+          respond([{"p1points": 5, 
+                    "p2points": 3,
+                    "p1games": 0,
+                    p2games: 1,
+                    "serving": 0
+                  }]);
  
       scope = $rootScope.$new();
       ctrl = $controller('ScorerCtrl', {$scope: scope});
     }));
  
  
-    it('should create "phones" model with 2 phones fetched from xhr', function() {
-      expect(scope.message).toEqual(undefined);
+    it('should create "score" model with scores fetched from xhr', function() {
+      expect(scope.score).toEqual(undefined);
       $httpBackend.flush();
  
-      expect(scope.message).toEqualData({ gameOdd : false });
+      expect(scope.score).toEqualData({"p1points": 5, 
+                                        "p2points": 3,
+                                        "p1games": 0,
+                                        "p2games": 1,
+                                        "serving": 0
+                                      });
     });
  
  
-    // it('should set the default value of orderProp model', function() {
-    //   expect(scope.orderProp).toBe('age');
-    // });
+    it('should set p2games on ScorerCtrl scope to 1', function() {
+      $httpBackend.flush();
+      expect(scope.p2games).toBe(1);
+    });
   });
  
  
