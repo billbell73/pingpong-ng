f8ff8378d853689709d143b821b551339b022826
Will Bell <willbell@gmail.com>
2014-01-31 21:00:19 +0000

    Added first hg-click attribute plus fn in ctrllr

--- app/scripts/controllers.js
+++ app/scripts/controllers.js
@@ -6,13 +6,11 @@ pingpongControllers.controller('ScorerCtrl', ['$scope', 'Data',
 
 		Data.query(function(data){
 	 		$scope.score = data[0];
-	 		$scope.p1points = data[0].p1points;
-	 		$scope.p2points = data[0].p2points;
-			$scope.p1games = data[0].p1games;
-			$scope.p2games = data[0].p2games;
-			$scope.server = data[0].server;
-			$scope.gameOdd = data[0].gameOdd;
 	 	})
+
+	 	$scope.increment = function(number){
+	 		alert('Increment for player ' + number);
+	 	}
 
 	}
 ]);
