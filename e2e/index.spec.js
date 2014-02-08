2e898a5bf0f16768dacdb31929e609c993c0f58d
Will Bell <willbell@gmail.com>
2014-02-08 13:02:08 +0000

    Refactored e2e updating points scores tests

--- e2e/index.spec.js
+++ e2e/index.spec.js
@@ -18,22 +18,6 @@ describe('E2E: Testing Scorer view', function() {
     expect(gameScore.getText()).toEqual('0');
   });
 
-  it('can increment a player1\'s point score', function() {
-    var p1Points = element(by.binding('score.p1points'));
-
-    p1Points.getText().then(function(scoreBefore){
-      p1Points.click();
-      p1Points.getText().then(function(scoreAfter){
-        expect(Number(scoreBefore) + 1).toEqual(Number(scoreAfter));
-      })
-    });	
-  });
-
-  it('can increment a player2\'s point score', function() {
-    var p2Points = element(by.binding('score.p2points'));
-    incrementTest(p2Points);
-  });
-
   function incrementTest(pointsElement) {
     pointsElement.getText().then(function(scoreBefore){
       pointsElement.click();
@@ -43,16 +27,37 @@ describe('E2E: Testing Scorer view', function() {
     }); 
   }
 
-  it('can decrement a player\'s score', function() {
-    var p2Points = element(by.binding('score.p2points'));
-    var p2Back = element(by.className('p2-back'));
+  it('can increment player1\'s point score', function() {
+    var p1Points = element(by.binding('score.p1points'));
+    incrementTest(p1Points);
+  });
 
-    p2Points.getText().then(function(scoreBefore){
-      p2Back.click();
-      p2Points.getText().then(function(scoreAfter){
+  it('can increment player2\'s point score', function() {
+    var p2Points = element(by.binding('score.p2points'));
+    incrementTest(p2Points);
+  });
+ 
+  function decrementTest(pointsElement, backElement) {
+    pointsElement.getText().then(function(scoreBefore){
+      backElement.click();
+      pointsElement.getText().then(function(scoreAfter){
         expect(Number(scoreBefore) - 1).toEqual(Number(scoreAfter));
       })
     }); 
+  }
+
+  it('can decrement player1\'s score', function() {
+    var p1Points = element(by.binding('score.p1points'));
+    var p1Back = element(by.className('p1-back'));
+
+    decrementTest(p1Points, p1Back);
+  });
+
+  it('can decrement player2\'s score', function() {
+    var p2Points = element(by.binding('score.p2points'));
+    var p2Back = element(by.className('p2-back'));
+
+    decrementTest(p2Points, p2Back);
   });
 
 });
