2e4def5cca29525625ae4e9a15af124395c199e9
Will Bell <willbell@gmail.com>
2014-04-22 19:34:51 +0100

    Refactor serveball directive

--- app/scripts/directives.js
+++ app/scripts/directives.js
@@ -1,17 +1,14 @@
 var pingpongDirectives = angular.module('pingpongDirectives', []);
 
-pingpongDirectives.directive('serveball1', function() {
-  return {
-    restrict: "E",
-    template: '<img id="p1marker" class="p1serveball"' + 
-    						' ng-show="score.currently_serving-1" src="images/serveball.svg">'
-  }
-})
+pingpongDirectives.directive('serveball', function() {
 
-pingpongDirectives.directive('serveball2', function() {
   return {
     restrict: "E",
-    template: '<img id="p2marker" class="p2serveball"' + 
-               'ng-show="score.currently_serving" src="images/serveball.svg">'
+    replace: true,
+    scope: {
+      pnumber: '@'
+    },
+    template: '<img id="p{{pnumber}}marker" class="p{{pnumber}}serveball"' + 
+               'src="images/serveball.svg">'
   }
 })
